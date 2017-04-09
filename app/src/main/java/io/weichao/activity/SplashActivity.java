5fb2f6832e9d6b75df37b682802c7931618818b8
weichao66666 <weichao66666@gmail.com>
2017-04-09 11:02:40 +0800

    Initial commit.

---
+++ app/src/main/java/io/weichao/activity/SplashActivity.java
@@ -0,0 +1,62 @@
+package io.weichao.activity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+import cn.easyar.samples.helloarvideo.MainActivity;
+import cn.easyar.samples.helloarvideo.R;
+import io.weichao.util.PermissionUtil;
+
+/**
+ * Created by WEI CHAO on 2017/3/27.
+ */
+
+public class SplashActivity extends Activity {
+    /**
+     * 请求码
+     */
+    private static final int REQUEST_CODE = 0;
+    /**
+     * 所需的全部权限
+     */
+    private static final String[] PERMISSIONS = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.INTERNET};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (Build.VERSION.SDK_INT >= 23 && PermissionUtil.isLackPermissions(this, PERMISSIONS)) {
+            //　Android 版本为6.0+，并且缺少权限, 进入权限配置页面
+            PermissionActivity.startActivityForResult(this, REQUEST_CODE, PERMISSIONS);
+        } else {
+            onResumeContinue();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // 拒绝时, 关闭页面, 缺少主要权限, 无法运行
+        if (requestCode == REQUEST_CODE && resultCode == PermissionActivity.PERMISSION_DENIED) {
+            finish();
+        } else {
+            onResumeContinue();
+        }
+    }
+
+    public void onResumeContinue() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
